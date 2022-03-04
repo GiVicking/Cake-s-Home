b7b3317739d048d9183a3e55bf152387994bd6ac
Gislaine <gikacarvalho85@hotmail.com>
2022-03-03 23:48:07 -0300

    Adicionado novas funcionalidades e novas imagens

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('button').click(function() {
+    $('#div1').fadeToggle('slow');
+    $('#div2').fadeToggle('slow');
+    $('#div3').fadeToggle('slow');
+    $('#div4').fadeToggle('slow');
+  });
+});
+
